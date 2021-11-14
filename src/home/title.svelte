9a797721219e7784481513872aecdd873533f396
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-14 19:52:07 +0800

    added main title component

---
+++ src/home/title.svelte
@@ -0,0 +1,21 @@
+<script></script>
+
+<main>
+  <h1>Hi, my name is Hakimi.</h1>
+  <h1>Please call me Kimi (pronounced Key Me).</h1>
+  <h3>I am software developer</h3>
+</main>
+
+<style>
+  main {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  h1 {
+    font-size: 3rem;
+    margin: 0;
+  }
+</style>
