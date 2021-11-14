653df1666d234334c345c0609adb2497b265f8a6
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-14 15:27:46 +0800

    triangle position changed

--- src/shape/triangle.svelte
+++ src/shape/triangle.svelte
@@ -1,5 +1,24 @@
-<script></script>
+<script>
+  function changeColor() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+    console.log("hi", randomColor);
+  }
+</script>
 
 <main>
-  <div>testing</div>
+  <div on:click={changeColor} class="arrow-down" />
 </main>
+
+<style>
+  .arrow-down {
+    position: absolute;
+    left: -200px;
+    width: 0;
+    height: 0;
+    border-left: 200px solid transparent;
+    border-right: 200px solid transparent;
+    border-top: 200px solid teal;
+    transform: rotate(75deg);
+  }
+</style>
