5c8f343f2d3aee70e0408c4180bdc3a551e254f7
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-16 20:32:46 +0800

    night code start

--- src/shape/triangle.svelte
+++ src/shape/triangle.svelte
@@ -1,10 +1,25 @@
 <script>
   let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  let arrowId = document.getElementById("arrow-down");
+  let countToStick = 0;
 
   function changeColor() {
+    let arrowId = document.getElementById("arrow-down");
+    countToStick += 1;
     randomColor = Math.floor(Math.random() * 16777215).toString(16);
     arrowId.style.borderTop = `200px solid #${randomColor}`;
+
+    if (countToStick > 20) {
+      arrowId.style.transform = "rotate(75deg)";
+      arrowId.style.borderLeft = "200px solid transparent";
+      arrowId.style.borderRight = "200px solid transparent";
+      arrowId.style.borderTop = "200px solid transparent";
+    } else if (countToStick > 4) {
+      console.log(countToStick);
+      arrowId.style.transform = "rotate(0deg)";
+      arrowId.style.borderLeft = "250px solid transparent";
+      arrowId.style.borderRight = "250px solid transparent";
+      arrowId.style.borderTop = "250px solid transparent";
+    }
   }
 </script>
 
