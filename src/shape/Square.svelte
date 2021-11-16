e98dc0ac4c52e39279bf38f421d559a98e470010
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-16 21:40:34 +0800

    added square component

---
+++ src/shape/Square.svelte
@@ -0,0 +1,19 @@
+<script>
+  let x = 0;
+</script>
+
+<main>
+  <div />
+</main>
+
+<style>
+  main {
+    position: absolute;
+    right: -190px;
+  }
+  div {
+    background-color: teal;
+    width: 250px;
+    height: 250px;
+  }
+</style>
