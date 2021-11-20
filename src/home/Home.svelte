46a7cedfb3284cf8f5bff4b4cc23bc33e1180b2b
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-20 17:02:19 +0800

    added home section

---
+++ src/home/Home.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Square from "../shape/Square.svelte";
+  import Triangle from "../shape/Triangle.svelte";
+  import Title from "./Title.svelte";
+</script>
+
+<main>
+  <Triangle />
+  <Title />
+  <Square />
+</main>
+
+<style>
+  main {
+    /* position: absolute; */
+    /* width: 100vw;
+    height: 100vh; */
+  }
+</style>
