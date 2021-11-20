cd9c59625031df3bc10d79c605a9ed86f2ce56dd
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-20 17:01:54 +0800

    added contact page

---
+++ src/contact/Contact.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<main>
+  <h1>contact</h1>
+</main>
+
+<style>
+  main {
+    /* width: 100vw; */
+    /* height: 100vh; */
+    /* position: absolute; */
+    /* transform: translateX(200vw); */
+  }
+</style>
