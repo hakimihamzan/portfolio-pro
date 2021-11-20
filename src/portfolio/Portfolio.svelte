f60b743a06972bf44474ded9b703efeb7239ac02
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-20 17:03:00 +0800

    added porfolio section

---
+++ src/portfolio/Portfolio.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<main>
+  <h1>porfolio</h1>
+</main>
+
+<style>
+  main {
+    /* width: 100vw;
+    height: 100vh;
+    position: absolute;
+    transform: translateX(100vw); */
+  }
+</style>
