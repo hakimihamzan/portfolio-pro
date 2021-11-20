cf0a22769852680f2da34f0c62bac2f915ac9ed3
hakimihamzan <hakimihamzan20@gmail.com>
2021-11-20 17:01:33 +0800

    added about page

---
+++ src/about/About.svelte
@@ -0,0 +1,14 @@
+<script></script>
+
+<main>
+  <h1>about</h1>
+</main>
+
+<style>
+  main {
+    /* width: 100vw;
+    height: 100vh;
+    position: absolute;
+    transform: translateX(300vw); */
+  }
+</style>
